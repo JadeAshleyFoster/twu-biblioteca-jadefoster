0eaa7fabf8a6b5e47929e3892f22ffef1c610a48
JadeAshleyFoster <jadefoster29@gmail.com>
2015-06-07 14:22:34 +0100

    Prints small booklist with title, author and year published

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,13 +1,26 @@
 package com.twu.biblioteca;
+import java.util.ArrayList;
 
 public class BibliotecaApp {
+    private static final int MAX_DETAIL_LENGTH = 23;
+    ArrayList<Book> bookList;
 
     public static void main(String[] args) {
         BibliotecaApp bib = new BibliotecaApp();
         bib.go();
     }
 
+    public void setBookList(ArrayList<Book> bookList) {
+        this.bookList = bookList;
+    }
+
     public void go() {
+        bookList = new ArrayList<Book>();
+        bookList.add(new Book("Perdido Street Station", "China Mieville", 2000));
+        bookList.add( new Book("Snow Crash", "Neal Stephenson", 1992));
+        bookList.add( new Book("The Nature of Code", "Daniel Shiffman", 2012));
+        setBookList(bookList);
+
         printWelcome();
     }
 
@@ -16,8 +29,10 @@ public class BibliotecaApp {
         printTableLine();
         System.out.println(getHeaders());
         printTableLine();
-        System.out.println(getListOfBooks());
-        printTableLine();
+        if(!bookList.isEmpty()) {
+            System.out.println(getListOfBooks());
+            printTableLine();
+        }
     }
 
     public String getWelcomeMessage() {
@@ -25,9 +40,45 @@ public class BibliotecaApp {
     }
 
     public String getListOfBooks() {
-        return "# Perdido Street Station  #\n# China Mieville          #\n# 2000                    #\n" +
-               "# Snow Crash              #\n# Neal Stephenson         #\n# 1992                    #\n" +
-                "# The Nature of Code      #\n# Daniel Shiffman         #\n# 2012                    #";
+        String listOfBooks = "";
+        for (Book book:bookList) {
+            listOfBooks += getFormattedBookDetails(book);
+        }
+        return listOfBooks.substring(0, listOfBooks.length()-1);    //to format next line
+    }
+
+    private String getFormattedBookDetails(Book book) {
+        String title = formatDetail(book.getTitle());
+        String author = formatDetail(book.getAuthor());
+        String yearPublished = formatDetail(Integer.toString(book.getYearPublished()));
+        return "# " + title + " # " + author + " # " + yearPublished + " #\n";
+    }
+
+    private String formatDetail(String detail) {
+        if (detailIsTooLong(detail)) {
+            return shortenDetail(detail);
+        }
+        return formatDetailLength(detail);
+    }
+
+    private String formatDetailLength(String detail) {
+        return detail += getJustifier(MAX_DETAIL_LENGTH - detail.length());
+    }
+
+    private String getJustifier(int justifierLength) {
+        String justifier = "";
+        for (int i = 0; i < justifierLength; i++) {
+            justifier += " ";
+        }
+        return justifier;
+    }
+
+    private String shortenDetail(String detail) {
+        return detail = detail.substring(0, MAX_DETAIL_LENGTH - 3) + "...";
+    }
+
+    private boolean detailIsTooLong(String detail) {
+        return detail.length() > MAX_DETAIL_LENGTH;
     }
 
     public String getHeaders() {
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -4,6 +4,7 @@ package com.twu.biblioteca;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
 
 public class BibliotecaAppTest {
     BibliotecaApp bibTest;
@@ -11,6 +12,11 @@ public class BibliotecaAppTest {
     @Before
     public void setUp() {
         bibTest = new BibliotecaApp();
+        ArrayList<Book> testBookList = new ArrayList<Book>();
+        testBookList.add(new Book("Perdido Street Station", "China Mieville", 2000));
+        testBookList.add( new Book("Snow Crash", "Neal Stephenson", 1992));
+        testBookList.add( new Book("The Nature of Code", "Daniel Shiffman", 2012));
+        bibTest.setBookList(testBookList);
     }
 
     @Test
@@ -22,9 +28,9 @@ public class BibliotecaAppTest {
     @Test
     public void testListOfBooks() {
         String output = bibTest.getListOfBooks();
-        assertEquals("# Perdido Street Station  #\n# China Mieville          #\n# 2000                    #\n" +
-                "# Snow Crash              #\n# Neal Stephenson         #\n# 1992                    #\n" +
-                "# The Nature of Code      #\n# Daniel Shiffman         #\n# 2012                    #", output);
+        assertEquals("# Perdido Street Station  # China Mieville          # 2000                    #\n" +
+                "# Snow Crash              # Neal Stephenson         # 1992                    #\n" +
+                "# The Nature of Code      # Daniel Shiffman         # 2012                    #", output);
     }
 
     @Test
